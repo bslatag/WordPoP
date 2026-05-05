7abfe03a917d556577cd9607c808c1c0842333bd
bslatag <2035570770@qq.com>
2026-05-06 04:33:19 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/study')
+def study():
+    df = pd.read_excel("word.xls")
+    word_list = df.to_dict('records')
+    random.shuffle(word_list)
+    return render_template('study.html', words=word_list)
+
+@app.route('/writing')
+def writing():
+    return render_template('writing.html')
+
+@app.route('/writing/practice')
+def writing_practice():
+    return render_template('writing_practice.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
