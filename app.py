97df43a848cab4e385d8f49d9fb6959ab78e3484
bslatag <2035570770@qq.com>
2026-05-06 11:06:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,8 @@ def writing():
 def writing_practice():
     return render_template('writing_practice.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
